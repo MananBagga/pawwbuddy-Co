19e3dba67239b3ce9fdd615f3fc4d378df322ccb
Manan Bagga <mananbagga0310@gmail.com>
2025-06-01 15:52:31 +0530

    add API error and response

--- src/index.js
+++ src/index.js
@@ -6,7 +6,19 @@ dotenv.config({
 })
 
 connectDB()
-
+.then(() => {
+    app.on("error", (error) => {
+        console.log("error: ", error);
+        throw error
+    })
+    
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port ${process.env.PORT}`);  
+    })
+})
+.catch((error) => {
+    console.log("MongoDB Connection failed: ", error);
+})
 
 
 
