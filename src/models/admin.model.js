a5f5da377d443023eaf9d7817cc3f874e0233347
Manan Bagga <mananbagga0310@gmail.com>
2025-06-09 10:39:59 +0530

    add jwt token method

--- src/models/admin.model.js
+++ src/models/admin.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt"
 
 const adminSchema = new mongoose.Schema({
     fullname: {
@@ -29,5 +30,40 @@ const adminSchema = new mongoose.Schema({
         ref: "Booking",
     }]
 }, {timestamps: true})
+
+adminSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next()
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+adminSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function (){
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullname: this.fullname
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+    )
+}
+userSchema.methods.generateRefreshToken = function (){
+    return jwt.sign({
+        _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+    )
+}
 
 export const Admin = new mongoose.model("Admin", adminSchema)
